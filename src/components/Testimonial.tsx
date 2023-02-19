78eb7b45ae93e0034169c5eafaa16f4101833982
shivam <shivampal7207@gmail.com>
2023-02-19 20:42:14 +0530

    testimonials added

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -7,7 +7,11 @@ import style from '../styles/Testimonial.module.css'
 function Testimonial() {
   return (
     <div className='bg-slate-900 min-h-[28rem] w-full py-4 flex flex-col overflow-hidden gap-4'>
-      <div className={`${style.animation_1} relative h-full flex flex-row gap-2`}>
+      <div className='text-center'>
+        <h1 className='text-white text-[3.4rem]'>300,000+ lives transformed</h1>
+        <p className='text-white text-[1.4rem]'>Real people, real stories - that's what <b>IDEATOFIT</b>is all about!</p>
+      </div>
+      <div className={`${style.animation_1} relative h-full flex flex-row gap-4`}>
         <Image src={img1} alt={'img1'} height={200} />
         <Image src={img2} alt={'img2'} height={200} />
         <Image src={img1} alt={'img1'} height={200} />
@@ -20,7 +24,7 @@ function Testimonial() {
         <Image src={img1} alt={'img1'} height={200} />
         <Image src={img1} alt={'img1'} height={200} />
       </div>
-      <div className={`${style.animation_2} relative h-full flex flex-row left-[-100%] gap-2`}>
+      <div className={`${style.animation_2} relative h-full flex flex-row left-[-100%] gap-4`}>
         <Image src={img1} alt={'img1'} height={200} />
         <Image src={img1} alt={'img1'} height={200} />
         <Image src={img1} alt={'img1'} height={200} />
