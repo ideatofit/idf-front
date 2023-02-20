d461d81955c45b417149a68c51a4ed26588a5887
shivam <shivampal7207@gmail.com>
2023-02-20 17:12:53 +0530

    80% completed ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import Slide from '@/components/Slider';
+import Slide from '@/components/Slide';
 import Navigation from '@/components/Navigation';
 import arrow1 from '../../public/arrow1.svg'
 import arrow2 from '../../public/arrow2.svg'
