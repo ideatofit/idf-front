0ac6173ecfcc249a7ff57a0f3d538bda7b0f33da
shivam <shivampal7207@gmail.com>
2023-02-27 16:47:51 +0530

    added admin

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,24 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <script async src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <script dangerouslySetInnerHTML={{
+            __html: `
+              if (window.netlifyIdentity) {
+                window.netlifyIdentity.on("init", user => {
+                  if (!user) {
+                    window.netlifyIdentity.on("login", () => {
+                      document.location.href = "/admin/";
+                    });
+                  }
+                });
+              }
+          `}}/>
       </body>
     </Html>
   )
