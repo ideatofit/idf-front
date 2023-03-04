5be2d08b032d5bca7118a4a17d890d9b8c8a9d09
shivam <shivampal7207@gmail.com>
2023-03-04 23:57:37 +0530

    stories words overflow fixed

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,22 +1,21 @@
 import React from 'react'
 import Image from 'next/image'
 import quote from '../../public/quote.svg'
-import { StoriesProps } from '@/lib/stories'
 
 function Stories(props:{
   text: string,
   name: string
 }) {
   return (
-          <div className='bg-[#232631] xl:min-h-[60vh] max-sm:h-[90vh] max-sm:max-h-[40vh] max-sm:min-w-[80vw] max-w-[45vw] min-w-[45vw]  flex items-center justify-center rounded-[1.5rem] border-borderColor border-2'>
+          <div className='bg-[#232631] xl:min-h-[60vh] max-sm:h-[95vh] max-sm:max-h-[40vh] max-sm:min-w-[100vw] max-w-[45vw] flex items-center justify-center rounded-[1.5rem] border-borderColor border-2'>
           <div className='h-fit w-[90%] p-4 rounded-xl flex flex-col'>
-            <div className='flex-[33%] justify-start max-md:pt-3 pl-16'>
+            <div className='flex-[33%] justify-start max-md:pt-3 pl-6'>
               <Image src={quote} alt="quote svg"/>
             </div>
-            <div className='flex-[33%] text-left px-16 py-4'>
+            <div className='flex-[33%] text-left px-4 py-4'>
               {props['text']}
             </div>
-            <div className='flex-[33%] text-left pl-16'>
+            <div className='flex-[33%] text-left pl-6'>
               {props['name']}
             </div>
           </div>
