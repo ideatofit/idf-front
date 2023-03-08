d55023e1beeedb11c86dd56620fd168e5ae5ca22
shivam <shivampal7207@gmail.com>
2023-03-08 08:57:51 +0530

    error in sign in resolved

--- src/pages/diets/index.tsx
+++ src/pages/diets/index.tsx
@@ -1,29 +1,16 @@
 import Footer from '@/layouts/Footer'
 import Navigation from '@/layouts/Navigation'
 import getFooterData, { FooterProps } from '@/lib/footer'
-import { useSession, signIn, signOut } from "next-auth/react"
 
 
 function Index(props: {
   footer: FooterProps
 }) {
-  const { data: session } = useSession()
   return (
     <div className='h-screen w-full bg-backgroundColor text-themeColor'>
       <Navigation />
       <div className='h-screen w-full bg-inherit grid place-items-center'>
-
-        {session ?
-          <div>
-            Signed in as {session.user.email} <br />
-            <button onClick={() => signOut()}>Sign out</button>
-          </div>
-          :
-          <div>
-            Not signed in <br />
-            <button onClick={() => signIn()}>Sign in</button>
-          </div>
-        }
+        Diets
       </div>
       <Footer footer={props['footer']} />
     </div>
