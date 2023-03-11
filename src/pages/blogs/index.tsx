93b55d7b240904f3aa65f682b5fe0b4a6af5d3b4
shivam <shivampal7207@gmail.com>
2023-03-12 00:16:22 +0530

    key provided to the blogscard

--- src/pages/blogs/index.tsx
+++ src/pages/blogs/index.tsx
@@ -2,28 +2,22 @@ import Blogscard from '@/components/Blogscard'
 import Footer from '@/layouts/Footer'
 import Navigation from '@/layouts/Navigation'
 import getFooterData, { FooterProps } from '@/lib/footer'
-import Link from 'next/link'
-import blogsimg from '../../../public/blogsimg.png'
 import { PostsProps, getPostsData } from '@/lib/posts'
-import { useEffect } from 'react'
 import Gotaquestion from '@/components/Gotaquestion'
 
 function Index(props: {
   footer: FooterProps
   posts: PostsProps
 }) {
-  useEffect(()=>{
-    console.log(props['posts'])
-  }, [])
   return (
     <>
     <div className='max-h-fit min-h-screen w-full bg-backgroundColor grid place-items-center text-themeColor'>
       <Navigation />
       <div className='h-fit w-full grid grid-cols-3 p-24'>
         {
-          props['posts']['posts'].map((data)=>{
+          props['posts']['posts'].map((data, i)=>{
             return(
-              <Blogscard img={data['img']} title={data['title']} description={data['description']} slug={data['slug']} publishedOn={data['publisdedat']}/>
+              <Blogscard key={`blogs${i}`} img={data['img']} title={data['title']} description={data['description']} slug={data['slug']} publishedOn={data['publisdedat']}/>
             )
           })
         }
